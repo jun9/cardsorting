2f50398438d172c62f3b759c85e50a7788a35d89
Yong <zhangyong@gmail.com>
2013-12-31 17:48:15 +0000

    update interface and plot.

--- server.R
+++ server.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(reshape2)
 library(vegan)
+library(ggdendro)
+library(ape)
 
 shinyServer(function(input, output) {
   data <- reactive({
@@ -13,9 +15,7 @@ shinyServer(function(input, output) {
     return(csv)  
   })
   
-  output$table <- renderTable({
-  data()  
-  })
+  output$table <- renderTable(data())
   
   cs.hc <-reactive({
     csv<-data()
@@ -24,23 +24,32 @@ shinyServer(function(input, output) {
     cs=melt(csv, measure.vars=1:ncol(csv))
     colnames(cs)=c('label', 'std.cat')
     t=dcast(cs, label ~ std.cat, length)
-    cs.dist =vegdist(t[,-1], method='jaccard')
-    cs.hc=hclust(cs.dist, method='ward')
+    if (input$hc_dist=='jaccard')
+      cs.dist =vegdist(t[,-1], method='jaccard')
+    else
+      cs.dist= dist(t[,-1], method=input$hc_dist)
+    cs.hc=hclust(cs.dist, method=input$hc_meth)
     cs.hc$labels=t[,1]
     return(cs.hc)
   })
   
   output$grouptable= renderDataTable({
-    t=cutree(cs.hc(), k=1:input$groups)
+    t=cutree(cs.hc(), k=1:input$groupt)
     t=cbind(row.names(t),t)
   })
   
   output$plot <- renderPlot({
-    plot(cs.hc(), family='Courier') 
+    plot(cs.hc(), family='serif', cex=.8,  xlab='cardsorting') 
     rect.hclust(cs.hc(), k=input$groups, border="red")
   })
   
+  output$dendroplot <- renderPlot({
+    #plot(as.phylo(cs.hc()), cex = 1)
+    print(ggdendrogram(cs.hc(), rotate=T, cex=.8, theme_dendro = F))
+  })
+
   output$summary <- renderPrint({
-    summary(data())
+    summary(data(), maxsum=100)
   })
 })
+
--- ui.R
+++ ui.R
@@ -1,40 +1,62 @@
 library(shiny)
 
-shinyUI(pageWithSidebar(
+  hc_method <- list("Ward's" = "ward", "Single" = "single", "Complete" = "complete", "Average" = "average", 
+                      "McQuitty" =  "mcquitty", "Median" = "median", "Centroid" = "centroid")
+  hc_dist_method <- c("jaccard", "euclidean", "maximum", "manhattan", "canberra", "binary", "minkowski")
+
+  shinyUI(pageWithSidebar(
   headerPanel("CardSorting Analysis App"),
   sidebarPanel(
-    tags$h5('Try with your own cardsorting result now!'),
+    tags$h5('Try it with your own cardsorting data now!'),
     tags$a('Download the example datasheet template here.', href='test.csv'),
-    
-    numericInput("groups", "Number of groups to explore:", 10),
+    tags$a("Download Donna Spencer's example datasheet.", href='iasumit.example.std.csv'),    
+    tags$a("(Her original Card sort analysis spreadsheet here.)", href="http://rosenfeldmedia.com/blogs/card-sorting/card-sort-analysis-spreadsheet/"),
     tags$hr(),
-    fileInput('file1', 'Choose CSV File',
+    fileInput('file1', 'Upload CSV File in the above template format only. 
+              Internet Explorer 9 and older browsers not supported.',
               accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
-    checkboxInput('header', 'Header', TRUE),
-    radioButtons('sep', 'Separator',
-                 c(Comma=',',
-                   Semicolon=';',
-                   Tab='\t'),
-                 'Comma'),
-    radioButtons('quote', 'Quote',
-                 c(None='',
-                   'Double Quote'='"',
-                   'Single Quote'="'"),
-                 'Double Quote'),
+    helpText('Options: Use only when you know what you are doing'),
+    checkboxInput('customCSV', 'Custom CSV format/encoding options', F),
     
-    radioButtons('encoding', 'Encoding',
-                 c(
-                   'UTF-8'='UTF-8',
-                   'GB2312'="GB2312",
-                   'BIG-5'="BIG-5"),
-                   'GB2312')
+    conditionalPanel(
+        condition = 'input.customCSV',
+        checkboxInput('header', 'With header row', T),
+        selectInput('sep', 'Separator',
+                    c(Comma=',',
+                      Semicolon=';',
+                      Tab='\t'),
+                    'Comma'),
+        selectInput('quote', 'Quote',
+                    c(None='',
+                      'Double Quote'='"',
+                      'Single Quote'="'"),
+                    'Double Quote'),
+        selectInput('encoding', 'Encoding',
+                    iconvlist(),
+                    'GB2312')
+      ),
+    
+    checkboxInput('customHC', 'Custom distance measure/clustering options', F),
+    conditionalPanel(
+        condition='input.customHC',
+        selectInput("hc_dist", label = "Distance measure:", choices = hc_dist_method, selected = hc_dist_method[1], multiple = FALSE),
+        selectInput("hc_meth", label = "Method:", choices = hc_method, selected = hc_method[1], multiple = FALSE)
+    )
+    #todo: custom dist and hc methods
   ),
   mainPanel(
     tabsetPanel(
       tabPanel("Table", tableOutput("table")),
       tabPanel("Summary", verbatimTextOutput("summary")), 
-      tabPanel("Hierarchical Clustering Dendrogram", plotOutput("plot")),
-      tabPanel("Groups", dataTableOutput("grouptable"))
+      tabPanel("Dendrogram", plotOutput("dendroplot", height=900)),
+      tabPanel("Explore clusters",  
+               numericInput("groups", "Choose the number of groups/clusters:", 7 ), 
+               plotOutput("plot")),
+      tabPanel("Group members", 
+               numericInput("groupt", "Choose the number of groups/clusters:", 7 ), 
+               dataTableOutput("grouptable"))
+      #Todo: download this group membership table.
     )
     )
 ))
+
